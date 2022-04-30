4108ad3595524a16ec21845b222a343dbe683a83
Andres Estrada <andrescross1616@hotmail.com>
2022-04-30 18:32:24 -0500

    navbar-footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { SobrenosotrosComponent } from './sobrenosotros/sobrenosotros.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { NovedadesComponent } from './novedades/novedades.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { InicioComponent } from './inicio/inicio.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobrenosotrosComponent,
+    ServiciosComponent,
+    NovedadesComponent,
+    ContactoComponent,
+    InicioComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'inicio', component: InicioComponent},
+      {path: 'servicios', component: ServiciosComponent},
+      {path: 'sobrenosotros', component: SobrenosotrosComponent},
+      {path: 'novedades', component: NovedadesComponent},
+      {path: 'contacto', component: ContactoComponent}
+    
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
