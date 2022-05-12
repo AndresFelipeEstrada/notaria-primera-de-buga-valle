98659a39380f63ee1e324263436f40a70606d200
Andres Felipe Estrada <andresfe1396@gmail.com>
2022-05-12 12:51:10 -0500

    barra-contacto.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'notaria-primera-de-buga-valle';
 
+
+  constructor(private router:Router) {}
+
+  Inicio(){
+    this.router.navigate(["inicio"])
+  }
+  Servicios(){
+    this.router.navigate(["servicios"])
+  }
+  SobreNosotros(){
+    this.router.navigate(["sobrenosotros"])
+  }
+  Novedades(){
+    this.router.navigate(["novedades"])
+  }
+  Contacto(){
+    this.router.navigate(["contacto"])
+  }
 }
 
 
