fafdde9325686780f28e7b8cd3aa2ebe08258b88
TheZheref <67617925+TheZheref@users.noreply.github.com>
2022-05-12 20:16:01 -0500

    Add files via upload
    Se agrego carrusel de imagenes

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -12,4 +12,25 @@ export class InicioComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  imgCollection: Array<object> = [
+    {
+      image: '/assets/IMG.JPG',
+      thumbImage: '/assets/IMG.JPG',
+    }, {
+      image: '/assets/IMG6.JPG',
+      thumbImage: '/assets/IMG6.JPG',
+    }, {
+      image: '/assets/IMG2.JPG',
+      thumbImage: '/assets/IMG2.JPG',
+    }, {
+      image: '/assets/IMG3.JPG',
+      thumbImage: '/assets/IMG3.JPG',
+    }, {
+      image: '/assets/IMG4.JPG',
+      thumbImage: '/assets/IMG4.JPG',
+    }, {
+      image: '/assets/IMG5.JPG',
+      thumbImage: '/assets/IMG5.JPG',
+    }
+];
 }
