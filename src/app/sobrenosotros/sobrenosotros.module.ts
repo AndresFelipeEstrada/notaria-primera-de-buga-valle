53c1be4b5be2a32a4a8c84502e77a760d44064f3
Deivy1102 <105081904+Deivy1102@users.noreply.github.com>
2022-05-12 21:05:06 -0500

    Modulo sobrenosotros

---
+++ src/app/sobrenosotros/sobrenosotros.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SobrenosotrosComponent } from './sobrenosotros.component';
+
+
+
+@NgModule({
+  declarations: [SobrenosotrosComponent],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    SobrenosotrosComponent
+
+  ]
+})
+export class SobrenosotrosModule { }
